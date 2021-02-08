340b3bf42343ebe6734af3eb71a69f2e36a737fb
lcfsih <1371735777@qq.com>
2021-02-08 14:54:02 +0800

    upd: app.js

--- lcfsih-blog/releases/20200809154032/js/app.js
+++ lcfsih-blog/releases/20200809154032/js/app.js
@@ -83,7 +83,7 @@ webpackJsonp([1], {
                 headBackImg: "https://cjunn.gitee.io/blog_theme_atum/img/ing/autorbimg.jpg",
                 bigBackImg: "https://cjunn.gitee.io/blog_theme_atum/img/body/background.jpg",
                 aboutmeHtml: "<img src='https://cjunn.gitee.io/blog_theme_atum/img/ing/aboutme.jpg'/>",
-				// aboutCouple: "<div class='textwidget'><div id='lovenyh' style='width: 100%; height: 100px; text-align: center; font-size: 1rem;'> <div id='lovenyhImage' style='width: 220px; margin: 0 auto;'><img src='https://yjlaugus.gitee.io/blog/img/body/ll.jpg' alt='love' style='width: 60px; border-radius: 50%;'><img src='https://yjlaugus.gitee.io/blog/img/body/z.webp' alt='love' style='width: 60px; border-radius: 50%;'><img src='https://yjlaugus.gitee.io/blog/img/body/nn.jpg' alt='love' style='width: 60px; border-radius: 50%;'></div><p id='elapseClock' style='font-size: 0.8rem;    margin-top: 16px;  background: linear-gradient(to right, red, blue);-webkit-background-clip: text;color: transparent;'>我们相恋了(刷新页面看天数呀)</p></div></div>",
+                // aboutCouple: "<div class='textwidget'><div id='lovenyh' style='width: 100%; height: 100px; text-align: center; font-size: 1rem;'> <div id='lovenyhImage' style='width: 220px; margin: 0 auto;'><a href='https://yjlaugus.gitee.io/loveniuniu/' target='_blank'><img src='https://yjlaugus.gitee.io/blog/img/body/ll.jpg' alt='love' style='width: 60px; border-radius: 50%;'></a><a href='https://yjlaugus.gitee.io/loveniuniu/' target='_blank'><img src='https://yjlaugus.gitee.io/blog/img/body/z.webp' alt='love' style='width: 60px; border-radius: 50%;'></a><a href='https://yjlaugus.gitee.io/loveniuniu/' target='_blank'><img src='https://yjlaugus.gitee.io/blog/img/body/nn.jpg' alt='love' style='width: 60px; border-radius: 50%;'></a></div><p id='elapseClock' style='font-size: 0.8rem;    margin-top: 16px;  background: linear-gradient(to right, red, blue);-webkit-background-clip: text;color: transparent;'>我们相恋了(刷新页面看天数呀)</p></div></div>",
                 blogUrlPre: "https://www.cnblogs.com/",
 					faceIcon: [{
                     name: "头条",
@@ -104,6 +104,12 @@ webpackJsonp([1], {
                     maxNum: 79,
                     file: ".png",
                     placeholder: "#emoji_{alias}#"
+                }, {
+                    name: "咦~",
+                    path: o + "/img/face/bili/",
+                    maxNum: 26,
+                    file: ".webp",
+                    placeholder: "#bili_{alias}#"
                 }],
                 autoInfoReset: !0,
                 openMathJax: !1,
@@ -215,7 +221,7 @@ webpackJsonp([1], {
                     }, [i("router-link", {
                         staticClass: "blog-name head-brand-color",
                         attrs: {
-                            to: "/",
+                            to: "https://www.cnblogs.com/lcfsih/",
                             tag: "span"
                         }
                     }, [i("div", {
@@ -851,7 +857,7 @@ webpackJsonp([1], {
                     })
                 },
                 loadArticleNum: function() {
-                    return it("/" + g.blogAcc + "/tag/", function(t) {
+                    return it("/" + g.blogAcc + "/ajax/blogStats/", function(t) {
                         return {
                             pageNum: parseInt((t.find("#stats_post_count").html() || "").replace("随笔", "").replace("-", "").trim()),
                             commentNum: parseInt((t.find("#stats-comment_count").html() || "").replace("评论", "").replace("-", "").trim())
@@ -3015,7 +3021,7 @@ webpackJsonp([1], {
                             attrs: {
                                 title: e.title
                             }
-                        }, [t._v(t._s(e.title))]), t._v(" "), i("div", {
+                        }, [t._v(t._s(e.title).replace(/^[0-9]+\.+/g,""))]), t._v(" "), i("div", {
                             staticClass: "item-see"
                         }, [i("span", {
                             staticClass: "icon iconfont see"
@@ -3052,7 +3058,7 @@ webpackJsonp([1], {
                             attrs: {
                                 title: e.title
                             }
-                        }, [t._v(t._s(e.title))]), t._v(" "), i("div", {
+                        }, [t._v(t._s(e.title).replace(/^[0-9]+\. +[eR]+\:+/g,""))]), t._v(" "), i("div", {
                             staticClass: "item-body double-ellipsis",
                             attrs: {
                                 title: e.body
